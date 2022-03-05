df082d445a67994835b451bda397a5ffbcced8fc
DiazRock <corolariodiaz@gmail.com>
2022-03-05 11:01:56 -0500

    Changed config

--- config/settings/base.py
+++ config/settings/base.py
@@ -163,6 +163,9 @@ CELERYD_TASK_SOFT_TIME_LIMIT = 60
 
 # Django REST Framework
 REST_FRAMEWORK = {
+   'DEFAULT_PERMISSION_CLASSES': [
+       'rest_framework.permissions.AllowAny'
+   ],
     'DEFAULT_RENDERER_CLASSES': (
         'rest_framework.renderers.JSONRenderer',
     ),
